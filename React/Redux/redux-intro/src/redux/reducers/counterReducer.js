a62cd67025d2366378b2f7d3adca1fed83bc8bde
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-06-04 14:57:43 +0300

    reducers and store added

---
+++ React/Redux/redux-intro/src/redux/reducers/counterReducer.js
@@ -0,0 +1,17 @@
+import * as actionsTypes from "../actions/actionsTypes";
+
+const counterReducer = (state = 0, action) => {
+  let newState;
+  switch (action.type) {
+    case actionsTypes.INCREASE_COUNTER:
+      return (newState = state + action.payload);
+    case actionsTypes.DECREASE_COUNTER:
+      return (newState = state - action.payload);
+    case actionsTypes.INCREASE_BY_TWO_COUNTER:
+      return (newState = state + action.payload);
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
