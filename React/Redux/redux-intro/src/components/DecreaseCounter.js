3f2b6036c5f962fc1399a57ac1239fdb51200277
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-06-04 19:05:08 +0300

    IncreaseByTwoCounter and Decrease comp. added redux

--- React/Redux/redux-intro/src/components/DecreaseCounter.js
+++ React/Redux/redux-intro/src/components/DecreaseCounter.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
-
-export default class DecreaseCounter extends Component {
+import { bindActionCreators } from "redux";
+import { decreaseCounter } from "../redux/actions/counterActions";
+import { connect } from "react-redux";
+class DecreaseCounter extends Component {
     render() {
         return (
             <div>
-                
+                <button
+          onClick={(e) => {
+            this.props.dispatch(decreaseCounter());
+          }}
+        >
+          -1
+        </button>
             </div>
         )
     }
 }
+//Aksiyon bağlamak için
+function mapDispatchToProps(dispatch) {
+    //counterActions içerisindeki increaseCounterı çalıştırmak istediğimizi belirtiyoruz.
+    return { actions: bindActionCreators(decreaseCounter, dispatch) };
+  }
+  
+  //DecreaseCounter comp. reduxa connect ediyoruz.
+  export default connect(mapDispatchToProps)(DecreaseCounter);
+  
--- React/Redux/redux-intro/src/components/IncreaseByTwoCounter.js
+++ React/Redux/redux-intro/src/components/IncreaseByTwoCounter.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react'
-
-export default class IncreaseByTwoCounter extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { increaseByTwoCounter } from "../redux/actions/counterActions";
+import { connect } from "react-redux";
+class IncreaseByTwoCounter extends Component {
+  render() {
+    return <div>
+        <button
+          onClick={(e) => {
+            this.props.dispatch(increaseByTwoCounter());
+          }}
+        >
+          +2
+        </button>
+    </div>;
+  }
 }
+
+//Aksiyon bağlamak için
+function mapDispatchToProps(dispatch) {
+  //counterActions içerisindeki increaseCounterı çalıştırmak istediğimizi belirtiyoruz.
+  return { actions: bindActionCreators(increaseByTwoCounter, dispatch) };
+}
+
+//IncreaseByTwoCounter comp. reduxa connect ediyoruz.
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter);
