c32d9ca91bdaf51fce50dcf1feb90e58751cfc3c
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-06-08 17:45:32 +0300

    CategoryList connect Redux

--- React/Redux/northwind-redux/src/components/categories/CategoryList.js
+++ React/Redux/northwind-redux/src/components/categories/CategoryList.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class CategoryList extends Component {
-    render() {
-        return (
-            <div>
-                <h3>CategoryList</h3>
-            </div>
-        )
-    }
+class CategoryList extends Component {
+  render() {
+    return (
+      <div>
+        <h3>CategoryList</h3>
+        <h5>Seçili Kategory : {this.props.currentCategory.categoryName}</h5>
+      </div>
+    );
+  }
 }
+
+//Stateti proplara bağla
+function mapStateToStore(state) {
+  return {
+      currentCategory: state.changeCategoryReducer
+  };
+}
+
+export default connect(mapStateToStore)(CategoryList);
