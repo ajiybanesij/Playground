3405509a71250b52e616c7378db20a704b74375c
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-06-08 15:50:37 +0300

    changeCategoryReducer added

---
+++ React/Redux/northwind-redux/src/redux/reducers/changeCategoryReducer.js
@@ -0,0 +1,10 @@
+import * as actionTypes from "../actions/actionTypes";
+import initialState from "./initialState";
+export default function changeCategoryReducer(state = initialState.cureentCategory,action) {
+  switch (action.type) {
+    case actionTypes.CHANGE_CATEGORY:
+      return action.payload;
+    default:
+      return state;
+  }
+}
