0430e6c20c258c39f67da07862eaa167c404bbba
Ajiyba Nesij <ajiybanesij@hotmail.com>
2020-06-12 18:42:23 +0300

    Redux getCompanies added

--- React/Redux/northwind-redux/src/components/categories/CategoryList.js
+++ React/Redux/northwind-redux/src/components/categories/CategoryList.js
@@ -1,38 +1,55 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import {bindActionCreators} from 'redux';
-import * as categoryActions from '../../redux/actions/categoryActions';
+import { bindActionCreators } from "redux";
+import { ListGroup, ListGroupItem } from "reactstrap";
+import * as categoryActions from "../../redux/actions/categoryActions";
 
 class CategoryList extends Component {
-    componentDidMount() {
-        this.props.actions.getCategories()
-    }
+  componentDidMount() {
+    this.props.actions.getCategories();
     
+  }
+
   render() {
     return (
       <div>
         <h3>CategoryList {this.props.categories.length}</h3>
         <h5>Seçili Kategory : {this.props.currentCategory.categoryName}</h5>
+        <ListGroup>
+          {this.props.categories.map(category => (
+            <ListGroupItem
+              active={category.id === this.props.currentCategory.id}
+              onClick={() => this.selectCategory(category)}
+              key={category.id}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
       </div>
     );
   }
 }
 
-//Stateti proplara bağla
-function mapStateToStore(state) {
+function mapStateToProps(state) {
   return {
-      currentCategory: state.changeCategoryReducer,
-      categories:state.categoryListReducer
+    currentCategory: state.changeCategoryReducer,
+    categories: state.categoryListReducer
   };
 }
 
-//actionları propslara bağla
-function mapDispachToProps(dispathc){
-    return{
-        actions:{
-            getCategories:bindActionCreators(categoryActions.getCategories,dispathc)
-        }
-    }
+function mapDispatchToProps(dispatch) {
+  return {
+    actions: {
+      getCategories: bindActionCreators(
+        categoryActions.getCategories,
+        dispatch
+      )}
+  };
 }
 
-export default connect(mapStateToStore,mapDispachToProps)(CategoryList);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CategoryList);
+
--- React/Redux/northwind-redux/src/redux/reducers/index.js
+++ React/Redux/northwind-redux/src/redux/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import changeCategoryReducer from "./changeCategoryReducer";
-import categoryListReducer from './changeCategoryReducer';
+import categoryListReducer from './categoryListReducer';
 //Başka reducer gelirse buranın içine ekliycez
 const rootReducer = combineReducers({
   changeCategoryReducer,
